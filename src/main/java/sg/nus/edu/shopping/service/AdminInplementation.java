ee6b56da93da541a15bcc36dc00d47d614347565
MaxC <maxcen@qq.com>
2024-10-05 16:41:16 +0800

    using homePage.html for pull Conflicts demo and added AdminInplementation.java

---
+++ src/main/java/sg/nus/edu/shopping/service/AdminInplementation.java
@@ -0,0 +1,24 @@
+package sg.nus.edu.shopping.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sg.nus.edu.shopping.interfacemethods.AdminInterface;
+import sg.nus.edu.shopping.model.Admin;
+import sg.nus.edu.shopping.repository.AdminRepository;
+
+import java.util.ArrayList;
+
+@Service
+@Transactional
+public class AdminInplementation implements AdminInterface {
+
+    @Autowired
+    AdminRepository adminRepository;
+
+    @Override
+    @Transactional
+    public ArrayList<Admin> findAdminByuserName(String userName) {
+        return null;
+    }
+}
