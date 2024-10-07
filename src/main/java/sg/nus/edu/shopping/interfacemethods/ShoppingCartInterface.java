eabb49177d7d55b27a663b5bd3c649788cefbe2b
hannah <hannah.wong@u.nus.edu>
2024-10-07 14:25:12 +0800

    update Shoppingcartinterface methods

--- src/main/java/sg/nus/edu/shopping/interfacemethods/ShoppingCartInterface.java
+++ src/main/java/sg/nus/edu/shopping/interfacemethods/ShoppingCartInterface.java
@@ -1,6 +1,13 @@
 package sg.nus.edu.shopping.interfacemethods;
 import sg.nus.edu.shopping.model.ShoppingCart;
 
+import java.util.List;
+
 
 public interface ShoppingCartInterface {
+    List<ShoppingCart> getCartByCustomerId(int customerId);
+    List<ShoppingCart> getCartbyCustomerUsername(String username);
+    ShoppingCart updateQuantity(int cartId, int quantity);
+    void removeProduct(int cartId);
+    ShoppingCart addProduct(String customerId, int productId, int quantity);
 }
