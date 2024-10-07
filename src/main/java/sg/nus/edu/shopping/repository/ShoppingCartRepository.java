f9c1a0498304d6b039dfaebe40425e3b520a9513
Jingyu Deng <89288939+chuhongxu@users.noreply.github.com>
2024-10-07 20:53:24 +0800

    Update ShoppingCartRepository.java

--- src/main/java/sg/nus/edu/shopping/repository/ShoppingCartRepository.java
+++ src/main/java/sg/nus/edu/shopping/repository/ShoppingCartRepository.java
@@ -6,10 +6,16 @@ import sg.nus.edu.shopping.model.Customer;
 import sg.nus.edu.shopping.model.ShoppingCart;
 
 import java.util.List;
-
+@Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
+    List<ShoppingCart> findByProductId(int productId);
     List<ShoppingCart> findByCustomerId(String customerId);
     List<ShoppingCart> findByCustomerUserName(String username);
     ShoppingCart getByCartId(int cartId);
     Customer findCustomerByCartId(int cartId);
 }
+
+
+
+
+
